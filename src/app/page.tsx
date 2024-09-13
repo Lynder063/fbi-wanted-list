1c5de7c7abd2ef07bad5ef4f0778ce86d04457f9
Lynder063 <lynderjekokot@gmail.com>
2024-09-14 00:04:42 +0200

    Cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,25 +164,6 @@ interface IconProps extends React.SVGProps<SVGSVGElement> {
   // Můžete přidat další vlastnosti, pokud je potřebujete
 }
 
-function FilterIcon(props: IconProps) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3" />
-    </svg>
-  );
-}
-
 function SearchIcon(props: IconProps) {
   return (
     <svg
